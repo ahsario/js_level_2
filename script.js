000fa648b4bcc84232d001388b4de4f17a14eb43
ahsario <amaru-gevara@mail.ru>
2021-03-14 12:50:03 +0300

    lesson 4

--- script.js
+++ script.js
@@ -30,15 +30,16 @@ class GoodsItem {
 
 class GoodsList {
   constructor() {
-    this.api = new ApiMock();
-    this.$goodsList = document.querySelector('.goods-list');
     this.goods = [];
+    this.filteredGoods = [];
   }
 
   fetchGoods() {
     return new Promise((resolve,reject) => {
       makeGETRequest(`${API_URL}/catalogData.json`).
-        then( goods => this.goods = goods,
+        then( goods => {
+          this.goods = goods;
+          this.filteredGoods = goods;},
           () => console.log('error')
         ).
         then(() => this.render())
@@ -51,19 +52,19 @@ class GoodsList {
   }
   render() {
     let listHtml = '';
-    this.goods.forEach(good => {
+    this.filteredGoods.forEach(good => {
       const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = listHtml;
   }
+  filterGoods(value) {
+    const regexp = new RegExp(value, 'i');
+    this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
+    this.render();
+  }
 }
-
-
-const goodsList = new GoodsList();
-goodsList.fetchGoods()
   
-
 function makeGETRequest(url) {
   return new Promise( (resolve, reject) => {
     var xhr;
@@ -109,7 +110,72 @@ class BascetItem {
   }
 }
 
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+
+document.querySelector('.search-button').addEventListener('click', (e) => {
+  const value = document.querySelector('.goods-search').value;
+  goodsList.filterGoods(value);
+})
+
 let bascket = new Bascket;
 bascket.getBascketItem()
 bascket.addBascetItem()
 bascket.sumPrice()
+
+// 1
+// let str = "dfgh 'hjkl' rftgyhuji 'ghj' gghj's ";
+// const regexp = /'/gi;
+// str.replace(regexp, '"');
+
+// 2
+// let str = "dfgh 'hjkl' rftgyhuji 'ghj' gghj's ";
+// const regexp = /'(\w+)'/gi;
+// str.replace(regexp, '"$1"');
+
+//3
+const regName = /^\b[a-z]+\b/gi,
+  regPhone = /^\+7\(\d{3}\)\d{3}-\d{4}/,
+  regMail = /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/,
+  regText = /^./;
+
+const nameInp = document.querySelector('#name'),
+  phoneInp = document.querySelector('#phone'),
+  mailInp = document.querySelector('#mail'),
+  submit = document.querySelector('#submit'),
+  text = document.querySelector('#text'),
+  form = document.querySelector('#form');
+
+const validation = (event) => {
+  event.preventDefault();
+  if (regName.test(nameInp.value)) {
+    nameInp.classList.remove('error')
+    nameInp.classList.add('valid')
+  } else {
+    nameInp.classList.remove('valid')
+    nameInp.classList.add('error')
+  }
+  if (regPhone.test(phoneInp.value)) {
+    phoneInp.classList.remove('error')
+    phoneInp.classList.add('valid')
+  } else {
+    phoneInp.classList.remove('valid')
+    phoneInp.classList.add('error')
+  }
+  if (regMail.test(mailInp.value)) {
+    mailInp.classList.remove('error')
+    mailInp.classList.add('valid')
+  } else {
+    mailInp.classList.remove('valid')
+    mailInp.classList.add('error')
+  }
+  if (regText.test(text.value)) {
+    text.classList.remove('error')
+    text.classList.add('valid')
+  } else {
+    text.classList.remove('valid')
+    text.classList.add('error')
+  }
+}
+
+submit.addEventListener('click', validation)

7086160c5872c16d1258f649dc4db75f45032126
ahsario <amaru-gevara@mail.ru>
2021-03-22 00:46:50 +0300

    lesson 6

--- script.js
+++ script.js
@@ -1,7 +1,78 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+Vue.component('filter-input', {
+  props: ['goods'],
+  template: `
+    <input type="text" class="goods-search"  @input="changes" />
+  `,
+  methods: {
+   changes(e) {
+        this.$emit('che', e) // Генерируем пользовательское событие
+      }
+  }
+})
+
+Vue.component('goods-list', {
+  props: ['goods', 'bascketGoods'],
+  template: `
+    <div class="goods-list" v-on:click="addHandler">
+      <goods-item v-for="good in goods" :good="good"></goods-item>
+    </div>
+  `,
+  methods: {
+    addHandler(e) {
+        this.$emit('add', e) 
+      }
+    }
+})
+
+Vue.component('goods-item', {
+  props: ['good'],
+  template: `
+    <div class="goods-item" :id="good.id_product">
+      <h3>{{ good.product_name }}</h3>
+      <p>{{ good.price }}</p>
+    </div>
+  `
+})
+
+Vue.component('bascket-list', {
+  props: ['goods'],
+  template: `
+    <div>
+      <button class="cart-button" @click="openCartHandler" type="button">Корзина</button>
+      <div class="bascket-list" v-if="isVisibleCart" v-on:click="removeHandler">
+        <h3>В вашей корзине:</h3>
+        <goods-list :goods="goods" />
+      </div>
+    </div>
+  `,
+  data() { 
+    return {
+      isVisibleCart: false
+    }
+  },
+  methods: {
+    openCartHandler() {
+      this.isVisibleCart = !this.isVisibleCart;
+    },
+
+    removeHandler(e) {
+      this.$emit('remove', e) // Генерируем пользовательское событие
+    }
+  }
+})
+
+Vue.component('error-handle', {
+  template: `
+    <h1>не удаётся выполнить запрос к серверу</h1>
+  `
+})
+
 const app = new Vue({
   el: '#app',
   data: {
+    isError: false,
     goods: [],
     filteredGoods: [],
     bascketGoods: [],
@@ -24,18 +95,38 @@ const app = new Vue({
         }
         xhr.open('GET', url, true);
         xhr.responseType = 'json';
-        xhr.send();
+        
         xhr.onreadystatechange = () => {
           if (xhr.readyState === 4) {
             resolve(xhr.response) 
-          }  
+          } 
         }
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
       })
     },
-    filterGoods() {
-      const regexp = new RegExp(this.searchLine, 'i');
+    filterGoods(e) {
+      const regexp = new RegExp(e.target.value, 'i');
       this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
-    }
+      console.log(e.target.value)
+    },
+    addToCartHandler(e) {
+      const id = e.target.closest('.goods-item').id;
+      console.log(id)
+      const good = this.goods.find((item) => item.id_product == id);
+
+      this.bascketGoods.push(good);
+      console.log(good)
+    },
+
+    removeFromCartHandler(e) {
+      console.log(e.target.closest('.goods-item').id)
+      const id = e.target.closest('.goods-item').id;
+      const goodIndex = this.bascketGoods.findIndex((item) => item.id == id);
+
+      this.bascketGoods.splice(goodIndex , 1);
+    },
+
   },
 
   mounted() {
@@ -43,36 +134,10 @@ const app = new Vue({
       then( goods => {
         this.goods = goods;
         this.filteredGoods = goods;
-        },
-        () => console.log('error')
-      )
+        console.log(JSON.stringify(goods));
+        }).
+      catch((e) => console.log(e))
   }
 
 });
-
-//   sumPrice() {
-//     let sum = 0;
-//     this.goods.forEach(item => sum += item.price);
-//     return sum;
-//   }
-  
-
-// class Bascket{
-//   constructor() {
-//     this.basketItemList = []
-//     this.sum = 0
-//   }
-//   getBascketItem() {
-//     makeGETRequest(`${API_URL}/getBasket.json`).then(data => console.log(data.contents));
-//   }
-//   addBascetItem() {
-//     makeGETRequest(`${API_URL}//addToBasket.json`).then(data => console.log(data));
-//   }
-//   deleteBascetItem() {
-//     makeGETRequest(`${API_URL}//deleteFromBasket.json`).then(data => console.log(data));
-//   }
-//   sumPrice() {
-//     makeGETRequest(`${API_URL}/getBasket.json`).then(data => console.log(data.amount));
-//   }
-// }
 
